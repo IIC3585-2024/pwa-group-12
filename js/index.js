cd34d398f4745d22d7f52a289eef28fafa1b881e
CristobalPerez-Cotapos <cprezcotapos@uc.cl>
2024-05-11 20:13:58 -0400

    sw okay

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 import { Person, Event } from './classes.js';
 import {addEvent} from './addEvent.js';
 import { startDB } from './IndeedDB.js';
-
+import { registerSW } from './workers.js';
 // definimos events en indeedDB API
+
+// Registrar el Service Worker
+await registerSW();
+
 startDB();
 
 // Agregar evento al hacer clic en el botón
