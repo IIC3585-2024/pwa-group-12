ef5257aea967d92ac9ae5d8188edc623af9f20c2
scavagnaro <sebastian@xpendit.com>
2024-05-12 02:12:46 -0400

    feat: :sparkles: format linter and added better suppor

--- js/IndeedDb.js
+++ js/IndeedDb.js
@@ -1,61 +1,113 @@
-const indeedDB = 
+const indeedDB =
   window.indexedDB ||
   window.mozIndexedDB ||
   window.webkitIndexedDB ||
   window.msIndexedDB ||
   window.shimIndexedDB;
 
-const request = indeedDB.open('Eventos', 1);
+const request = await indeedDB.open("events", 1);
 
 function startDB() {
   // Definimos multiples versiones de indexedDB, ya que no todos los navegadores soportan la misma versión
 
-
-  request.onerror = function(event) {
-    console.log('Error: No se pudo abrir la base de datos');
+  request.onerror = function (event) {
+    console.log("Error: No se pudo abrir la base de datos");
     console.log(event);
-  }
+  };
 
-  request.onupgradeneeded = function() {
-    const db = request.result;
-    console.log('Actualizando base de datos');
-    const store = db.createObjectStore("events", { keyPath: 'id', autoIncrement: true });
-    const name = store.createIndex('name', 'name', { unique: false });
-  }
+  request.onupgradeneeded = function () {
+    console.log("Actualizando la base de datos");
+    createEventStore();
+  };
 
-  request.onsuccess = function() {
-    console.log('Base de datos abierta');
-  }
+  request.onsuccess = function () {
+    console.log("Base de datos abierta");
+  };
+}
+
+function createEventStore() {
+  let db = request.result;
+  let eventStore = db.createObjectStore("events", {
+    keyPath: "id",
+    autoIncrement: true,
+  });
+  eventStore.createIndex("name", "name", { unique: false });
 }
 
 function saveEvent(event) {
   let db = request.result;
-  let transaction = db.transaction(["events"], 'readwrite');
-  let store = transaction.objectStore('events');
+  let transaction = db.transaction(["events"], "readwrite");
+  let store = transaction.objectStore("events");
   store.add(event);
-  console.log('Evento guardado');
+  console.log("Evento guardado");
+}
+
+async function getEvent(eventId) {
+  return new Promise((resolve, reject) => {
+    let requestHere = indexedDB.open("events", 1);
+
+    requestHere.onsuccess = function (event) {
+      let db = event.target.result;
+
+      var transaction = db.transaction(["events"], "readonly");
+      var store = transaction.objectStore("events");
+
+      var getRequest = store.get(parseInt(eventId));
+
+      getRequest.onsuccess = function (event) {
+        var event = getRequest.result; // Assign the result to the variable
+        console.log("Evento obtenido: ", event);
+        resolve(event); // Resolve the promise with the retrieved event data
+      };
+      getRequest.onerror = function (event) {
+        console.log("Error al obtener el evento: ", getRequest.error);
+        reject(getRequest.error); // Reject the promise with the error
+      };
+
+      transaction.oncomplete = function (event) {
+        db.close();
+      };
+    };
+
+    requestHere.onerror = function (event) {
+      console.log("Error al abrir la base de datos: ", requestHere.error);
+      reject(requestHere.error); // Reject the promise if there's an error opening the database
+    };
+  });
 }
 
 async function getEvents() {
-  let db = request.result;
-  let transaction = db.transaction('events', 'readonly');
-  let store = transaction.objectStore('events');
+  return new Promise((resolve, reject) => {
+    let requestHere = indexedDB.open("events", 1);
 
-  try {
-    // Esperar a que se complete la operación getAll
-    let getRequest = await new Promise((resolve, reject) => {
-      let request = store.getAll();
-      request.onsuccess = () => resolve(request);
-      request.onerror = () => reject(request.error);
-    });
+    requestHere.onsuccess = function (event) {
+      let db = event.target.result;
 
-    // Una vez que la operación getAll se completa, puedes acceder al resultado
-    console.log(getRequest.result);
-    return getRequest.result;
-  } catch (error) {
-    console.error('Error al obtener eventos:', error);
-    throw error; // Propagar el error para que se maneje fuera de esta función si es necesario
-  }
+      var transaction = db.transaction(["events"], "readonly");
+      var store = transaction.objectStore("events");
+
+      var getRequest = store.getAll();
+
+      getRequest.onsuccess = function (event) {
+        var event = getRequest.result; // Assign the result to the variable
+        console.log("Evento obtenido: ", event);
+        resolve(event); // Resolve the promise with the retrieved event data
+      };
+      getRequest.onerror = function (event) {
+        console.log("Error al obtener el evento: ", getRequest.error);
+        reject(getRequest.error); // Reject the promise with the error
+      };
+
+      transaction.oncomplete = function (event) {
+        db.close();
+      };
+    };
+
+    requestHere.onerror = function (event) {
+      console.log("Error al abrir la base de datos: ", requestHere.error);
+      reject(requestHere.error); // Reject the promise if there's an error opening the database
+    };
+  });
 }
 
-export { saveEvent, getEvents, startDB };
+export { saveEvent, getEvents, getEvent, startDB };
--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-import { Person, Event } from './classes.js';
-import {addEvent} from './addEvent.js';
-import { startDB } from './IndeedDB.js';
-import { registerSW } from './workers.js';
+import { addEvent, updateEventList } from "./addEvent.js";
+import { startDB } from "./IndeedDB.js";
+import { registerSW } from "./workers.js";
 // definimos events en indeedDB API
 
 // Registrar el Service Worker
 await registerSW();
 
 startDB();
+await updateEventList();
 
 // Agregar evento al hacer clic en el botón
-document.getElementById('addEventBtn').addEventListener('click', addEvent);
+document.getElementById("addEventBtn").addEventListener("click", addEvent);
